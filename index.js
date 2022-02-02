153e4a0c7a3af4a4abf9788d9be45fd6abd23bd6
algmerzs <algmerzs@gmail.com>
2022-02-01 20:56:21 -0500

    changes to deploy

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-#!/usr/bin/env node
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-/**
- * Module dependencies.
- */
+const app = express();
 
-var app = require("../app");
 var debug = require("debug")("crud:server");
 var http = require("http");
 
@@ -84,3 +87,45 @@ function onListening() {
     var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
     debug("Listening on " + bind);
 }
+
+const customersRouter = require("./src/routes/index");
+
+// view engine setup
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
+// middlewares
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "src/public")));
+
+// routes
+app.use("/", customersRouter);
+
+// dotenv
+dotenv.config({
+    path: "./env/.env",
+});
+
+// database
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
